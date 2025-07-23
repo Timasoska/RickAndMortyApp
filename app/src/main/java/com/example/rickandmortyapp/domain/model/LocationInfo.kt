83722b34db8f340eda44ef0f0c3c96aad0d4952c
Timofey <Tim005rud@gmail.com>
2025-07-23 16:02:43 +0300

    Добавил маппер и дата класс на уровне домена

--- app/src/main/java/com/example/rickandmortyapp/domain/model/LocationInfo.kt
+++ app/src/main/java/com/example/rickandmortyapp/domain/model/LocationInfo.kt
@@ -1,3 +1,9 @@
 package com.example.rickandmortyapp.domain.model
 
-data class LocationInfo()
+data class LocationInfo (
+    val id: Int,
+    val name: String,
+    val type: String,
+    val dimension: String,
+    val residentIds: List<Int>, //Получаем количество жителей
+)
--- app/src/main/java/com/example/rickandmortyapp/mappers/mappers.kt
+++ app/src/main/java/com/example/rickandmortyapp/mappers/mappers.kt
@@ -2,8 +2,10 @@ package com.example.rickandmortyapp.mappers
 
 import com.example.rickandmortyapp.data.dto.Character
 import com.example.rickandmortyapp.data.dto.Episode
+import com.example.rickandmortyapp.data.dto.Location
 import com.example.rickandmortyapp.domain.model.CharacterInfo
 import com.example.rickandmortyapp.domain.model.EpisodeInfo
+import com.example.rickandmortyapp.domain.model.LocationInfo
 
 
 fun Character.toDomainModel(): CharacterInfo{
@@ -24,5 +26,21 @@ fun Episode.toDomainModel(): EpisodeInfo {
         name = this.name,
         airDate = this.airDate,
         episode = this.episode
+    )
+}
+
+fun Location.toDomainModel(): LocationInfo {
+    return LocationInfo(
+        id = this.id,
+        name = this.name,
+        type = this.type,
+        dimension = this.dimension,
+        residentIds = this.residents.mapNotNull { url -> //Парсим url чтобы получить количество
+            if (url.isNotBlank()) {
+                url.substringAfterLast('/').toIntOrNull()
+            } else {
+                null
+            }
+        }
     )
 }
