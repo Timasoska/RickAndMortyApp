b17255e5c2de4a62c3e3f9a4c5bca356f7150200
Tim00 <Tim005rud@yandex.ru>
2025-08-15 12:59:19 +0300

    Добавил GetCharacterByIdUseCase

---
+++ app/src/main/java/com/example/rickandmortyapp/domain/useCase/GetCharacterByIdUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.rickandmortyapp.domain.useCase
+
+import com.example.rickandmortyapp.domain.repository.CharacterRepository
+import com.example.rickandmortyapp.utils.Resource
+import javax.inject.Inject
+
+class GetCharacterByIdUseCase @Inject constructor(private val repository: CharacterRepository) {
+    suspend operator fun invoke(id: Int) = repository.getCharacterById(id)
+}
